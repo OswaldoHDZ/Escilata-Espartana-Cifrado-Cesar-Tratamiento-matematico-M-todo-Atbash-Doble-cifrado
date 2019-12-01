7e85adb9daa892dfe3e83593f53adef34746065f
OswaldoHDZ <oswaldo.hernandez@estudiante.uacm.edu.mx>
2019-12-01 22:56:21 +0100

    Se agrego comentarios a las variables y funciones

--- Proyecto.c
+++ Proyecto.c
@@ -15,14 +15,14 @@ char buscarCesarCifrar(char buscado,char arrCesar[2][26]);
 char buscarCesarDeCifrar(char buscado,char arrCesar[2][26]);
 //--------------------------------------------------------------
 void tratamientoMatematico();
-char* convierteCadenaMayusculas(char cadena[],int longitudCadena);
+char* convierteCadenaMayusculas(char cadena[],int longitudCadena); //Se pone char* asterisco porque regresa un arreglo | Con char[] pero no se puede : https://foro.elhacker.net/programacion_cc/c_funcion_que_retorne_arrays-t84925.0.html
 int regresaValorLetra(char letra);
 int realizaOperacioIndicada(int valorLetra);
 char* imprimeCodificacionOperaMetetico(int arreglo[],int longitud, char cadenaCodificada[]);
 int realizaOperacioIndicadaDepeje(int valorLetra);
 //-------------------------------------------------------------- 
 void metodoAtbash();
-char* codificaBash(int arreglo[], int longitud,char cadenaCodificada[]);
+char* codificaBash(int arreglo[], int longitud,char cadenaCodificada[]); //Se pone char* asterisco porque regresa un arreglo | Con char[] pero no se puede : https://foro.elhacker.net/programacion_cc/c_funcion_que_retorne_arrays-t84925.0.html
 void dobleCifrado();
 //--------------------------------------------------------------
 char regresaSifradoUno(char buscado,char arr[3][26]);
@@ -65,31 +65,31 @@ void menu(){
 }
 //comienza Escilata Espartana
 /*----------------------------------------------------------------------------------------------------------------------------------------------*/
+//Esta función realiza el cifrado con escitalaEspartana
 void escitalaEspartana(){
-    int i,j;
-    int cont=0;
-    int limit=0;
-    int tamCad=0;
-    char escitalaEspartana1 [30];
-    char escitalaEspartana [3][9];
-    char decifradoEscitalaEspartana [3][9];
+    int i,j; //Contadores 
+    int cont=0; //Contador para recorrer la escitalaEspartana
+    int tamCad=0; //Tamaño de la cadea que lo vamos a obtener con strlen : http://www.forosdelweb.com/f96/obtener-tamano-array-dinamico-568584/
+    char escitalaEspartana1 [30];  //Esta es la escitala
+    char escitalaEspartana [3][9]; //Aqui vamos a meter los valores de la cadena uno por uno
+    char decifradoEscitalaEspartana [3][9]; //Aqui vamos a meter los valores cifrados
 
     //system("cls");
     for(i=0;i<30;i++){
         escitalaEspartana1[i]='*';
     }
-        for(i=0;i<3;i++){
-            for(j=0;j<9;j++){
-                escitalaEspartana [i][j]=escitalaEspartana1[cont];
-                cont++;
-            }
+    for(i=0;i<3;i++){
+        for(j=0;j<9;j++){
+            escitalaEspartana [i][j]=escitalaEspartana1[cont];
+            cont++;
         }
-        cont=0;
+    }
+    cont=0;
     fflush(stdin);
     printf("Ingresa Cadena\n");
     gets(escitalaEspartana1);
     tamCad = strlen(escitalaEspartana1);
-    if(espacioVaciosValidar(escitalaEspartana1,tamCad)==0) {
+    if(espacioVaciosValidar(escitalaEspartana1,tamCad)==0||verificaCadenaSinNumero==0) {
         for(i=0;i<3;i++){
             for(j=0;j<9;j++){
                 if(cont<tamCad){
@@ -140,16 +140,18 @@ void escitalaEspartana(){
         printf("\n");
     }else{
         printf("\n");
-        printf("La cadena tiene espacios");
+        printf("La cadena tiene espacios o numeros");
     }
     printf("\n");
 }
 //Termina Escilata Espartana
 /*----------------------------------------------------------------------------------------------------------------------------------------------*/
 //Comienza Cifrado Cesar
+//La siguiente función realiza el cifrado y decifrado Cesar | Contiene un menu para saber si quieres cifrar o decifrar un mensaje
 void cifradoCesar (){
     system("cls");
-    int n,j,i;
+    int n,j,i; //contadoes
+    //La siguiente matriz contiene la metriz del cifrado
     char arrCesar[2][26]={{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'},
                           {'X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W'}};
     
@@ -175,19 +177,19 @@ void cifradoCesar (){
         break;
     }
 }
-
+//La siguiente función Decifra un mensaje
 void cesarDecifrar(char arrCesar[2][26]){
 
-    int tam=0;
-    int k;
+    int tam=0; //contiene el tamaño de la cadena
+    int k;     //Es un contedor para recorrer la cadena
     fflush(stdin);//limpia el buffer si no marca error
-    char arr[tam];
+    char arr[tam]; //contiene la cadena
     printf("Introduce texto a cifrar:\n");
     gets(arr);
     fflush(stdin);
 
     tam = strlen(arr);
-   if(espacioVaciosValidar(arr,tam)==0){
+   if(espacioVaciosValidar(arr,tam)==0  || verificaCadenaSinNumero==0){
     printf("\n\n--Texto Introducido--");
     printf("\n> %s",arr);
     printf("\n\n--Texto Cifrado--");
@@ -198,16 +200,16 @@ void cesarDecifrar(char arrCesar[2][26]){
     }
     printf("\n");
     }else{
-                printf("Tienes espacios\n");
+                printf("Tienes espacios o números\n");
     }
 
 }
 
-
+//La siguiente función cifra un mensaje con el metodo cesar
 void cesarCifrar(char arrCesar[2][26]){
 
-    int tam=0;
-    int k;
+    int tam=0; //contiene el tamaño del arreglo
+    int k;    //Recorre el arreglo
     
     fflush(stdin);//limpia el buffer si no marca error
     char arr[tam];
@@ -230,10 +232,10 @@ void cesarCifrar(char arrCesar[2][26]){
                 printf("Tienes espacios\n");
     }
 }
-
+//Dea cuerdo a una letra la busca en la matriz de cesar, en cuanto encuetra la letra regresa la posición (letra) pero la que esta arriba de la matriz, una posición arriba
 char buscarCesarDeCifrar(char buscado,char arrCesar[2][26]){
-int i;
-char a='*';
+int i; //Para recorrer las filas de la matriz
+char a='*'; //Regresa la letra de la metriz
     for(i=0;i<26;i++){
       if( buscado==arrCesar[1][i]){
           a=arrCesar[0][i];
@@ -241,10 +243,10 @@ char a='*';
     }
 return a;
 }
-
+//Dea cuerdo a una letra la busca en la matriz de cesar, en cuanto encuetra la letra regresa la posición (letra) pero la que esta abajo de la matriz, una posición abajo
 char buscarCesarCifrar(char buscado,char arrCesar[2][26]){
-int i;
-char a='*';
+int i; //Para recorrer las filas
+char a='*'; //Regresa la letra de la metriz
     for(i=0;i<26;i++){
       if( buscado==arrCesar[0][i]){
           a= arrCesar[1][i];
@@ -255,14 +257,15 @@ return a;
 //Termina Cifrado cesar
 //comienza Tratamiento matemático
 /*----------------------------------------------------------------------------------------------------------------------------------------------*/
+//Esta funcuín lee una cadena y la cifra deacurdo al tratamiento matematicp
 void tratamientoMatematico(){
-    char cadena[100];
-    int i=0, longitudCadena;
-    char letraBuscar;
-    int valorLetra;
-    int nuevoValor;
-    int valoresDeCadenaCodificados[100];
-    int logitudDEArreglo_valoresDeCadenaCodificados;
+    char cadena[100]; //Aqui guardamos la cadena a cifrar
+    int i=0, longitudCadena; //i : contador, longitudCadena: guardamos la logitud de la cadena
+    char letraBuscar;       //La utilizaremos para saber que letra vamos sar su valor deacurdo a la tabla 
+    int valorLetra;         //Guardamos el valor de la letra
+    int nuevoValor;         //Realizamos la operación y tendra un nuevo valor para buscarlo en la tabla
+    int valoresDeCadenaCodificados[100]; //Aqui vamos a guardar los valores codificados
+    int logitudDEArreglo_valoresDeCadenaCodificados; //Vamos a calcular la logitud del arreglo que tiene los valores de las letras ya calculados
     printf("\n Estas en tratamiento matematico\n");
     printf("Ingresa Cadena ");
     gets(cadena);
@@ -292,8 +295,8 @@ void tratamientoMatematico(){
             printf("%d ",nuevoValor); //Si no quieres que imprima los valores, solo quita este printf
         }
         printf("\n");
-        char cadenaCodificada[longitudCadena];
-        char *cadenaCodi;
+        char cadenaCodificada[longitudCadena]; //La utilizaremos para decodificar la cadena
+        char *cadenaCodi;                      //La cadena en mayusculas, lo ponemos con char* porque sí lo reciviremos en la cunnción de realiza esto 
         cadenaCodi = imprimeCodificacionOperaMetetico(valoresDeCadenaCodificados,longitudCadena,cadenaCodificada);
         printf("Apartir de aqui camos adecodificar la cadena\n");
         for(i=0; i<longitudCadena;i++){
@@ -319,7 +322,6 @@ void tratamientoMatematico(){
 //Esta funcion dada una letra regresa el valor de la letra deacuerdo a la tabla que es ta en el PDF en el punto Tratamiento matemático
 int regresaValorLetra(char letra){
     char alfabeto[] ={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-
     int i;
     for (i=0;i<26;i++){
         if(letra == alfabeto[i]){ //Regresamos el valor de i porque es la posición correspondiente al valor de la letra
@@ -331,12 +333,13 @@ int regresaValorLetra(char letra){
 //Realizar la operación indicada: C = (15 + 3) (mód 26) = 18.
 //Deacuerdo al valor de la letra seralizamos la operación
 int realizaOperacioIndicada(int valorLetra){
-    int nuevoValor;
-    nuevoValor = (valorLetra + 3) % 26;
+    int nuevoValor; //Guardara el nuevo valor de la letra con operación
+    nuevoValor = (valorLetra + 3) % 26; 
     return nuevoValor;
 }
+//Calcula el valor inicial antes de que se realizara el tratamiento matematico
 int realizaOperacioIndicadaDepeje(int valorLetra){
-    int nuevoValor;
+    int nuevoValor; //Es la operación pero despejada para calcular el valor inicial
     nuevoValor = (valorLetra /  (1 % 26 )) -3;
     if(nuevoValor == -3)
         nuevoValor = 23;
@@ -348,8 +351,8 @@ int realizaOperacioIndicadaDepeje(int valorLetra){
 }
 //Decodifica el valor de cada letra
 char* imprimeCodificacionOperaMetetico(int arreglo[],int longitud, char cadenaCodificada[]){
-    int i = 0;
-    int j = 0;
+    int i = 0; //recorre el arrelgo
+    int j = 0; //recorre el arrelgo
     char alfabeto[] ={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     printf("La cadena codificada es: ");
     for(i=0; i< longitud; i++){
@@ -419,9 +422,10 @@ void metodoAtbash(){
         printf("Tienes espacios\n");
     }
 }
+//Regresa la codena codificada deacuerdo al metodo bash
 char* codificaBash(int arreglo[], int longitud,char cadenaCodificada[]){
-    int i = 0;
-    int j = 0;
+    int i = 0; //Recorre el vector
+    int j = 0; //Recorre el vector
     char alfabeto[] ={'Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A'};
     
     for(i=0; i< longitud; i++){
@@ -437,11 +441,11 @@ char* codificaBash(int arreglo[], int longitud,char cadenaCodificada[]){
 }
 //Termina Tratamiento Atbash
 //-----------------------------------------------------------------------------------------------------------------------------------------------
-
+//Realiza el doble cifrado de una cadena
 void dobleCifrado(){
-    int tam=0,i;
-    int p;
-    int q;
+    int tam=0,i; //i : recorre el vetor, tam: obitiene el tamaño del vector introducido
+    int p;      //Para recorrer la matriz 
+    int q;      //Para recorrer la matriz 
     char arrDobleCifrado[3][26]={{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'},
                                 {'F','R','J','O','Y','D','I','Q','T','Z','S','L','M','E','U','N','B','K','W','A','H','P','C','X','G','V'},
                                 {'H','T','R','V','Z','D','I','Q','J','Y','P','E','L','M','U','B','N','K','A','W','F','S','X','C','G','O'}};
@@ -510,7 +514,7 @@ printf("\n\n--Sifrado uno--");
             printf("Tienes espacios\n");
    }
 }
-
+//Obiene el valor de la letra, deacuerdo a su valor de abajo
 char regresaSifradoUno(char buscado,char arr[3][26]){
 int i;
 char a='*';
@@ -521,7 +525,7 @@ char a='*';
     }
 return a;
 }
-
+//Obiene el valor de la letra, deacuerdo a su valor de abajo
 char regresaSifradoDos(char buscado,char arr[3][26]){
 	int i;
 	char a='*';
@@ -532,6 +536,7 @@ char regresaSifradoDos(char buscado,char arr[3][26]){
 	    }
 	return a;
 }
+//Obiene el valor de la letra, deacuerdo a su valor de arriba
 char regresaDecifradoDos(char buscado,char arr[3][26]){
     int i;
 	char a='*';
@@ -542,6 +547,7 @@ char regresaDecifradoDos(char buscado,char arr[3][26]){
 	    }
 	return a;
 }
+//Obiene el valor de la letra, deacuerdo a su valor de arriba
 char regresaDecifradoUno(char buscado,char arr[3][26]){
     int i;
 	char a='*';
@@ -553,6 +559,7 @@ char regresaDecifradoUno(char buscado,char arr[3][26]){
 	return a;
 }
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//funcion que valida que la cadena no tenga espacios
 int espacioVaciosValidar(char a[],int tam ){
     int i,j=0;
     for(i=0;i<tam;i++){
@@ -571,6 +578,7 @@ char* convierteCadenaMayusculas(char cadena[],int longitudCadena){
 	}
     return cadena;
 }
+//Verifica que la cadena no tenga numeros
 int verificaCadenaSinNumero(char cadena[],int longitudCadena){
 	int i,j=0;
     for(i=0;i<longitudCadena;i++){
